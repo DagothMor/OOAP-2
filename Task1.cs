6ab9eb2ca28e34ad18533c725c29a92b296e75d2
siegf <siegf@DESKTOP-N3BL9TC>
2026-03-26 21:05:44 +0400

    task1

---
+++ Task1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP
+{
+    internal class Task1
+    {
+        public void Run()
+        {
+            var person = new Person();
+            var employee = new Employee();
+
+            // Пример полиморфизма.
+            sayName(employee);
+
+            // Example Usage
+            var myLibrary = new Library
+            {
+                Name = "Central Library",
+                BookShelves = new List<BookShelf>
+    {
+        new BookShelf
+        {
+            Number = 1,
+            Books = new List<Book>
+            {
+                new Book
+                {
+                    Title = "C# in Depth",
+                    Author = "Jon Skeet",
+                    YearPublished = 2019
+                },
+                new Book
+                {
+                    Title = "The Pragmatic Programmer",
+                    Author = "Andy Hunt & Dave Thomas",
+                    YearPublished = 1999
+                }
+            }
+        }
+    }
+            };
+        }
+
+        public void sayName(Person person)
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+
+
+    class Person
+    {
+        private string _name = "";
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    // Пример Наследования.
+    class Employee : Person
+    {
+
+    }
+
+    //___
+
+    // Пример композиции:
+    public class Library
+    {
+        public string Name { get; set; }
+
+        public List<BookShelf> BookShelves { get; set; } = new List<BookShelf>();
+    }
+
+    public class BookShelf
+    {
+        public int Number { get; set; }
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int YearPublished { get; set; }
+    }
+}
