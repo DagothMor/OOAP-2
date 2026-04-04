1564ce5eea1f3b7dd17f5f1b16c02effb50cc6e2
siegf <siegf@DESKTOP-N3BL9TC>
2026-04-04 23:55:52 +0400

    task 4

---
+++ Task4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP
+{
+    internal class Task4
+    {
+
+        // Reflexion: неправильно понял, надо было рассказать про открытые классы в библиотеках dll, и что подключаются через using.
+
+        // Task4: 
+
+        // (Close)OfficialDocument -> (Close)ContractualDocument -> (Open)SupAgreement
+        //                          -> (Close)ContractBase -> (Open)Contract
+
+        // Закрыто поскольку при обновлении ЕСМ платформы кастомные изменения клиентов могут спровоцировать конфликты.
+        // Логику и возможность наследования, разрешение на расширение лежит непосредственно на Вендоре, только он отвечает за границы полета фантазий будущих потребителей.
+    }
+}
