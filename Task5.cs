73ce9c874737f99343a188a22e15d26becb1a514
siegf <siegf@DESKTOP-N3BL9TC>
2026-04-07 23:59:42 +0400

    task 5

---
+++ Task5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP
+{
+    internal class Task5
+    {
+        // 1 - 2 - 3. Новый модуль может входить в семейство модулей, ориентированных на решение некоторой общей задачи, которую не удаётся решить с помощью одного модуля;
+        // да
+        // 4 - 5
+        // С Помощью рефлексии и source generator может быть? не уверен.
+    }
+}
