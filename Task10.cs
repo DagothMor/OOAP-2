b14bf7efb23a4be5bc5ea5d90ba71ea0b8a9f66e
siegf <siegf@DESKTOP-N3BL9TC>
2026-04-22 23:46:12 +0400

    Task10

---
+++ Task10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP
+{
+    public class Parent
+    {
+        public virtual void Display() { }
+    }
+    public class Child : Parent
+    {
+        // Переопределяем и запечатываем
+        public sealed override void Display() { }
+    }
+
+    public class GrandChild : Child
+    {
+        // Ошибка билда, пытаемся переопределить запечатанный метод.
+        public sealed override void Display() { }
+    }
+}
