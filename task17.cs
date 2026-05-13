3789d2dc20bb93201e576daf91bf85cab250fc4c
siegf <siegf@DESKTOP-N3BL9TC>
2026-05-14 00:07:43 +0400

    Task17

---
+++ task17.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP
+{
+    public class Task17
+    {
+        // В Шахте может быть герой, и она является крепостью, но во время боя, ни укреплений, ни стрелков ни рова.
+        // Ангел является Т7, но в преобразователе все равно будет считаться скелетом (робо-скелетом?)
+    }
+}
