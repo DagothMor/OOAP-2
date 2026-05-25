a97dc57615307fa1818dd315a62cf005b4dc3c5d
siegf <siegf@DESKTOP-N3BL9TC>
2026-05-25 18:26:29 +0400

    Task21

---
+++ Task21.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP
+{
+
+
+    // implementation inheritance
+    // Монстр Сколопендра унаследовала функции класса Список.
+    // Теперь, чтобы убить ее, значение головы, или всего тела должны быть равны 0
+    public class Scolopendra : List<int>
+    {
+
+    }
+
+    // facility inheritance
+    class Zero
+    {
+        public Zero() { }
+        public virtual void Fatality()
+        {
+            Console.WriteLine("Скучная анимация добивания противника");
+        }
+    }
+
+    class SubZero : Zero
+    {
+        public SubZero() { }
+        public override void Fatality()
+        {
+            Console.WriteLine("Ледяная анимация добивания противника");
+        }
+    }
+
+    class Scorpion : Zero
+    {
+        public Scorpion() { }
+        public override void Fatality()
+        {
+            Console.WriteLine("Огненная анимация добивания противника и признание в любви к SubZero");
+        }
+    }
+}
